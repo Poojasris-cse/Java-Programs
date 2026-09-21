f0fe40183007a26a2118038e8ff78d0e07fa3b07
POOJASRI S <poojasris022@gmail.com>
2026-09-21 09:34:56 +0530

    Add DuplicateElements.java to find duplicates in array

---
+++ Arrays/DuplicateElements.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class DuplicateElements {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            boolean duplicate = false;
+
+            for (int j = 0; j < i; j++) {
+                if (a[i] == a[j]) {
+                    duplicate = true;
+                    break;
+                }
+            }
+
+            if (duplicate)
+                continue;
+
+            for (int j = i + 1; j < n; j++) {
+                if (a[i] == a[j]) {
+                    System.out.println(a[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
