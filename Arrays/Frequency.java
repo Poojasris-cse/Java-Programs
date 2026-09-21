4e69c7ba5569e6dab11fdebb19dff72378732d23
POOJASRI S <poojasris022@gmail.com>
2026-09-21 09:31:34 +0530

    Add Frequency class to count occurrences of integers

---
+++ Arrays/Frequency.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Frequency {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            if (a[i] == -1)
+                continue;
+
+            int count = 1;
+
+            for (int j = i + 1; j < n; j++) {
+                if (a[i] == a[j]) {
+                    count++;
+                    a[j] = -1;
+                }
+            }
+
+            System.out.println(a[i] + " = " + count);
+        }
+    }
+}
